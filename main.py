cf6ef21ee42da5f6e4e220e1af05f5c119dd682f
dell <tianyu@qxslab.cn>
2020-12-21 12:34:14 +0800

    dcgan, best mae=0.146, folder dcgan

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 
 import os
 from math import floor
+import numpy as np
+from PIL import Image, ImageFont, ImageDraw
 import torch
 import torch.nn as nn
 from torch.nn.functional import l1_loss
@@ -14,6 +16,7 @@ from torchvision import transforms
 from torchvision.utils import make_grid, save_image
 from raw import PowerSet
 from model import CGAN_G, CGAN_D, weight_init
+from model import C_DCGAN_G, C_DCGAN_D
 
 
 def quality(img, match, metric):
@@ -25,7 +28,24 @@ def quality(img, match, metric):
     return metric(img.view(-1), match.view(-1))
 
 
-def visulize_weight_grad(writer, model, step):
+def display_quality(grid, qlty):
+
+    """
+        display quality in image grid
+    """
+
+    grid_np = grid.detach().cpu().numpy()
+    grid_np = np.uint8(grid_np.transpose(1, 2, 0)*255)
+    grid_pil = Image.fromarray(grid_np)
+    draw = ImageDraw.Draw(grid_pil)
+    font = ImageFont.truetype("/usr/share/fonts/truetype/"
+                              "dejavu/DejaVuSansMono.ttf", size=30)
+    draw.text((0, 0), "quality {0:.5f}".format(qlty), font=font)
+
+    return grid_pil
+
+
+def visualize_weight_grad(writer, model, step):
 
     """
         visualize weight and grad in tensorboard
@@ -37,9 +57,30 @@ def visulize_weight_grad(writer, model, step):
         writer.add_histogram("grad/"+tag, value.grad, step)
 
 
+def visualize_img(G, fixed_batch, metric, writer=None, msg=None):
+
+    """
+        visualize and qualify generated image
+    """
+
+    param, match = fixed_batch
+    noise = torch.rand(param.shape[0], 100).cuda()
+    img = G(noise, param).reshape(-1, 1, 64, 64)
+    fake_grid = make_grid(img[:64], normalize=True)
+
+    qlty = quality(img, match, metric)
+    qlty_grid = display_quality(fake_grid, qlty)
+
+    if not writer:
+        qlty_grid.save("best.png")
+    else:
+        qlty_grid = np.asarray(qlty_grid).transpose(2, 0, 1)
+        writer.add_image(msg, qlty_grid)
+
+
 def main():
 
-    trail = "format_code"
+    trail = "dcgan"
 
     experiment = "/home/dell/hdd/program_fsrpe/{}".format(trail)
 
@@ -73,16 +114,19 @@ def main():
     # check data
     fixed, real_batch = next(iter(tloader))
     fixed, real_batch = fixed.cuda(), real_batch.cuda()
-    grid_img = make_grid(real_batch[:64], normalize=True)
-    writer.add_image("real", grid_img)
+    real_grid = make_grid(real_batch[:64], normalize=True)
+    writer.add_image("real", real_grid)
 
     # setup loss
     criterion = nn.BCELoss()
 
     # setup model
-    G = CGAN_G().cuda()
-    D = CGAN_D().cuda()
+    # G = CGAN_G().cuda()
+    # D = CGAN_D().cuda()
+    G = C_DCGAN_G().cuda()
+    D = C_DCGAN_D().cuda()
 
+    # difine visualization model
     class CGAN(nn.Module):
         """
             for model visualization
@@ -112,6 +156,12 @@ def main():
     # record global step
     step = 0
 
+    # recored best result
+    best = float("inf")
+
+    # setup metric
+    metric = l1_loss
+
     for e in range(10000):
 
         iter_loader = iter(tloader)
@@ -135,33 +185,27 @@ def main():
             fake = G(noise, param).detach()
 
             # clear grad in D
-
             D.zero_grad()
 
             # grad of match
-
             flag = D(param, match)
             errD_match = criterion(flag, flag.new_ones(flag.shape))
             errD_match.backward()
 
             # grad of no_match
-
             flag = D(param, no_match)
             errD_no_match = criterion(flag, flag.new_zeros(flag.shape))
             errD_no_match.backward()
 
             # grad of fake
-
             flag = D(param, fake)
             errD_fake = criterion(flag, flag.new_zeros(flag.shape))
             errD_fake.backward()
 
             # update D
-
             D_opt.step()
 
             # grab data for G
-
             param, match = next(iter_loader, ("", ""))
             if param == "":
                 break
@@ -171,64 +215,62 @@ def main():
             match = match.cuda()
 
             # clear grad in G
-
             G.zero_grad()
 
             # grad of G
-
             img = G(noise, param)
             flag = D(param, img)
             errG = criterion(flag, flag.new_ones(flag.shape))
             errG.backward()
 
             # generation quality
-            metric = quality(img, match, l1_loss)
+            qlty = quality(img, match, metric)
 
             # update G
-
             G_opt.step()
 
             # update global step
-
             step += 1
+
             if step % 10 == 9:
-                continue
 
-            # track progress
+                # track progress
+                print("Epoch [{0:5d}] Global [{1:8d}] "
+                      "errD [{2:2.5f}/{3:2.5f}/{4:2.5f}] "
+                      "errG [{5:2.5f}]".format(e, step,
+                          errD_match, errD_no_match, errD_fake, errG))
 
-            print("Epoch [{}] Global [{}]".format(e, step), end="\r")
+                # track performance
+                errD = {"real": errD_match,
+                        "no_match": errD_no_match,
+                        "gen": errD_fake,
+                        "all": errD_match + errD_no_match + errD_fake}
+                writer.add_scalars("err/D", errD, step)
+                writer.add_scalar("err/G", errG, step)
+                writer.add_scalar("err/quality", qlty, step)
 
-            # track performance
+            if qlty < best:
 
-            errD = {"real": errD_match,
-                    "no_match": errD_no_match,
-                    "gen": errD_fake,
-                    "all": errD_match + errD_no_match + errD_fake}
-            writer.add_scalars("err/D", errD, step)
-            writer.add_scalar("err/G", errG, step)
-            writer.add_scalar("err/mae", metric, step)
+                best = qlty
 
-        # save model
+                # visualize best image
+                visualize_img(G, (fixed, real_batch), metric)
+
+                # save best model
+                torch.save(G.state_dict(),
+                           os.path.join(experiment, "G_best.pt"))
+                torch.save(D.state_dict(),
+                           os.path.join(experiment, "D_best.pt"))
 
         if e % 10 == 9:
 
-            # check weight
+            # track weight and grad
+            visualize_weight_grad(writer, G, step)
+            visualize_weight_grad(writer, D, step)
 
-            visulize_weight_grad(writer, G, step)
-            visulize_weight_grad(writer, D, step)
-
-            # check output
-
-            noise = torch.rand(fixed.shape[0], 100).cuda()
-            img = G(noise, fixed)
-            metric = quality(img, real_batch, l1_loss)
-            fake_img = make_grid(img[:64], normalize=True)
-            writer.add_image("fake/{0}/{1}".format(e, metric), fake_img)
-
-            torch.save(G.state_dict(),
-                       os.path.join(experiment, "G_{}.pt".format(e)))
-            torch.save(D.state_dict(),
-                       os.path.join(experiment, "D_{}.pt".format(e)))
+            # track image generation
+            visualize_img(G, (fixed, real_batch), metric,
+                          writer, "fake/{}".format(e))
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -107,3 +107,108 @@ class CGAN_D(nn.Module):
         flag = flag.view(-1)
 
         return flag
+
+
+class C_DCGAN_G(nn.Module):
+
+    """
+        generator of DCGAN cgan
+        ref https://github.com/znxlwm/pytorch-MNIST-
+        CelebA-cGAN-cDCGAN/blob/master/pytorch_cDCGAN.png
+    """
+
+    def __init__(self):
+
+        super(C_DCGAN_G, self).__init__()
+
+        self.deconv1_1 = nn.Sequential(
+            # (batch, 100, 1, 1)
+            nn.ConvTranspose2d(100, 256, 4, 1, 0),
+            nn.BatchNorm2d(256),
+            nn.ReLU())  # (batch, 256, 4, 4)
+
+        self.deconv1_2 = nn.Sequential(
+            # (batch, 4, 1, 1)
+            nn.ConvTranspose2d(4, 256, 4, 1, 0),
+            nn.BatchNorm2d(256),
+            nn.ReLU())  # (batch, 256, 4, 4)
+
+        self.main = nn.Sequential(
+            # (batch, 512, 4, 4)
+            nn.ConvTranspose2d(512, 256, 4, 2, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            # (batch, 256, 8, 8)
+            nn.ConvTranspose2d(256, 128, 4, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            # (batch, 128, 16, 16)
+            nn.ConvTranspose2d(128, 64, 4, 2, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            # (batch, 64, 32, 32)
+            nn.ConvTranspose2d(64, 1, 4, 2, 1),
+            nn.Tanh())  # (batch, 1, 64, 64)
+
+    def forward(self, noise, param):
+
+        noise = noise.reshape(-1, 100, 1, 1)
+        param = param.reshape(-1, 4, 1, 1)
+        feat1 = self.deconv1_1(noise)
+        feat2 = self.deconv1_2(param)
+        feature = torch.cat([feat1, feat2], dim=1)
+        img = self.main(feature)
+
+        return img
+
+
+class C_DCGAN_D(nn.Module):
+
+    """
+        discriminator for DCGAN cgan
+        ref https://github.com/znxlwm/pytorch-MNIST-
+        CelebA-cGAN-cDCGAN/blob/master/pytorch_cDCGAN.png
+    """
+
+    def __init__(self):
+
+        super(C_DCGAN_D, self).__init__()
+
+        self.conv1_1 = nn.Sequential(
+            # (batch, 1, 64, 64)
+            nn.Conv2d(1, 64, 4, 2, 1),
+            nn.LeakyReLU(0.2))  # (batch, 64, 32, 32)
+
+        self.conv1_2 = nn.Sequential(
+            # (batch, 4, 64, 64)
+            nn.Conv2d(4, 64, 4, 2, 1),
+            nn.LeakyReLU(0.2))  # (batch, 64, 32, 32)
+
+        self.main = nn.Sequential(
+            # (batch, 128, 32, 32)
+            nn.Conv2d(128, 256, 4, 2, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+            # (batch, 256, 16, 16)
+            nn.Conv2d(256, 512, 4, 2, 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+            # (batch, 512, 8, 8)
+            nn.Conv2d(512, 1024, 4, 2, 1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2),
+            # (batch, 1024, 4, 4)
+            nn.Conv2d(1024, 1, 4, 1, 0),
+            nn.Sigmoid())  # (batch, 1, 1, 1)
+
+    def forward(self, param, img):
+
+        feat1 = self.conv1_1(img)
+        param = param.reshape(-1, 4, 1, 1)
+        param = param.expand(-1, -1, 64, 64)
+        feat2 = self.conv1_2(param)
+        feature = torch.cat([feat1, feat2], dim=1)
+        flag = self.main(feature)
+        flag = flag.view(-1)
+
+        return flag
